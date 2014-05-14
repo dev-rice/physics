06d8c4944627bece8f3c20e0f6bd250c7a6e687e
chris <chris@platypus.(none)>
2014-05-14 01:27:48 -0500

    basic 3d drawing with spheres\!

--- makefile
+++ makefile
@@ -1,4 +1,2 @@
 all:
-	g++ -w -c src/*.cpp -I /home/chris/lib/SFML-2.1/include/
-	g++ *.o -o physics-app -L /home/chris/lib/SFML-2.1/lib/ -lsfml-graphics -lsfml-window -lsfml-system -lGLU
-	rm *.o
+	g++ -w src/*.cpp -lGL -lGLU -lglut -o physics_app
--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,10 @@
 #include <cmath>
 #include <cstdlib>
 #include <time.h>
-#include <SFML/Graphics.hpp>
-#include <SFML/OpenGL.hpp>
+
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
 
 const int WIDTH = 800;
 const int HEIGHT = 600;
@@ -20,250 +22,34 @@ Body random_body();
 double fRand(double, double);
 void make_solar_system(GravityHandler&);
 
-bool    fullscreen=false;   // Fullscreen Flag Set To Fullscreen Mode By Default
-bool    vsync=true;         // Turn VSYNC on/off
-bool    light;              // Lighting ON/OFF ( NEW )
+// OpenGL
+void reshape(int, int);
+void display(void);
+void draw_body(Body&);
 
-GLfloat xrot;               // X Rotation
-GLfloat yrot;               // Y Rotation
-GLfloat xspeed;             // X Rotation Speed
-GLfloat yspeed;             // Y Rotation Speed
-GLfloat z=-5.0f;            // Depth Into The Screen
+GravityHandler gravity_handler;
 
-GLfloat LightAmbient[]=     { 0.5f, 0.5f, 0.5f, 1.0f };
-GLfloat LightDiffuse[]=     { 1.0f, 1.0f, 1.0f, 1.0f };
-GLfloat LightPosition[]=    { 0.0f, 0.0f, 2.0f, 1.0f };
-
-GLuint  filter;             // Which Filter To Use
-GLuint  texture[3];         // Storage For 3 Textures
-
-int LoadGLTextures()                                    // Load Bitmaps And Convert To Textures
-{
-    int Status=false;                                   // Status Indicator
-
-    // Load The Bitmap, Check For Errors, If Bitmap's Not Found Quit
-    sf::Image Image;
-    if (Image.loadFromFile("companion_cube.bmp"))
-    {
-        Status=true;                                    // Set The Status To true
-
-        glGenTextures(3, &texture[0]);                  // Create Three Textures
-
-        // Create Nearest Filtered Texture
-        glBindTexture(GL_TEXTURE_2D, texture[0]);
-        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
-        glTexImage2D(GL_TEXTURE_2D, 0, 3, Image.getSize().x, Image.getSize().y, 0, GL_RGBA, 
-            GL_UNSIGNED_BYTE, Image.getPixelsPtr());
-
-        // Create Linear Filtered Texture
-        glBindTexture(GL_TEXTURE_2D, texture[1]);
-        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
-        glTexImage2D(GL_TEXTURE_2D, 0, 3, Image.getSize().x, Image.getSize().y, 0, GL_RGBA, 
-            GL_UNSIGNED_BYTE, Image.getPixelsPtr());
-
-        // Create MipMapped Texture
-        glBindTexture(GL_TEXTURE_2D, texture[2]);
-        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR_MIPMAP_NEAREST);
-        gluBuild2DMipmaps(GL_TEXTURE_2D, 3, Image.getSize().x, Image.getSize().y, GL_RGBA,
-            GL_UNSIGNED_BYTE, Image.getPixelsPtr());
-    }
-
-    return Status;                                      // Return The Status
-}
-
-GLvoid ReSizeGLScene(GLsizei width, GLsizei height)     // Resize And Initialize The GL Window
-{
-    if (height==0)                                      // Prevent A Divide By Zero By
-    {
-        height=1;                                       // Making Height Equal One
-    }
-
-    glViewport(0,0,width,height);                       // Reset The Current Viewport
-
-    glMatrixMode(GL_PROJECTION);                        // Select The Projection Matrix
-    glLoadIdentity();                                   // Reset The Projection Matrix
-
-    // Calculate The Aspect Ratio Of The Window
-    gluPerspective(45.0f,(GLfloat)width/(GLfloat)height,0.1f,100.0f);
-
-    glMatrixMode(GL_MODELVIEW);                         // Select The Modelview Matrix
-    glLoadIdentity();                                   // Reset The Modelview Matrix
-}
-
-int InitGL()                                            // All Setup For OpenGL Goes Here
-{
-    if (!LoadGLTextures())                              // Jump To Texture Loading Routine
-    {
-        return false;                                   // If Texture Didn't Load Return false
-    }
-
-    glEnable(GL_TEXTURE_2D);                            // Enable Texture Mapping
-    glShadeModel(GL_SMOOTH);                            // Enable Smooth Shading
-    glClearColor(0.0f, 0.0f, 0.0f, 0.5f);               // Black Background
-    glClearDepth(1.0f);                                 // Depth Buffer Setup
-    glEnable(GL_DEPTH_TEST);                            // Enables Depth Testing
-    glDepthFunc(GL_LEQUAL);                             // The type Of Depth Testing To Do
-    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);  // Really Nice Perspective Calculations
-
-    glLightfv(GL_LIGHT1, GL_AMBIENT, LightAmbient);     // Setup The Ambient Light
-    glLightfv(GL_LIGHT1, GL_DIFFUSE, LightDiffuse);     // Setup The Diffuse Light
-    glLightfv(GL_LIGHT1, GL_POSITION,LightPosition);    // Position The Light
-    glEnable(GL_LIGHT1);                                // Enable Light One
-    return true;                                        // Initialization Went OK
-}
-
-void drawCube(float x, float y, float z){
-    glPushMatrix();
-    glTranslatef(x/10 , y/10, z);
-
-    glRotatef(xrot,1.0f,0.0f,0.0f);
-    glRotatef(yrot,0.0f,1.0f,0.0f);
-
-    glBindTexture(GL_TEXTURE_2D, texture[0]);
-
-    glBegin(GL_QUADS);
-        // Front Face
-        glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);
-        glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);
-        glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f,  1.0f);
-        glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f,  1.0f);
-        // Back Face
-        glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
-        glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);
-        glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);
-        glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f, -1.0f);
-        // Top Face
-        glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);
-        glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f,  1.0f,  1.0f);
-        glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f,  1.0f,  1.0f);
-        glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);
-        // Bottom Face
-        glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
-        glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f, -1.0f, -1.0f);
-        glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);
-        glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);
-        // Right face
-        glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, -1.0f, -1.0f);
-        glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);
-        glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f,  1.0f,  1.0f);
-        glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);
-        // Left Face
-        glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
-        glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);
-        glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f,  1.0f,  1.0f);
-        glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);
-    glEnd();
-
-    glPopMatrix();
-}
-
-int DrawGLScene(GravityHandler& handler){               // Here's Where We Do All The Drawing
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear The Screen And The Depth Buffer
-    glLoadIdentity();                                   // Reset The View
-    
-    for (int i = 0; i < handler.get_bodies().size(); ++i){
-    	Body a = handler.get_bodies()[i];
-    	Vector position = a.get_position();
-    	//printf("%d: ", i);
-    	//position.print();
-    	drawCube(position.x, position.y, position.z);
-    }
-
-    xrot+=xspeed;
-    yrot+=yspeed;
-    return true;                                        // Keep Going
-}
-
-int main() {
+int main(int argc, char **argv) {
 
 	srand(time(NULL));
-
-	sf::ContextSettings settings;
-	settings.antialiasingLevel = 4;		
-
-	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Gravity", sf::Style::None, settings);
-	window.setFramerateLimit(60);
 	
-	GravityHandler gravity_handler;
+	//GravityHandler gravity_handler;
 	populate_handler(gravity_handler);
-	gravity_handler.set_time_multiplier(1);
+	gravity_handler.set_time_multiplier(2.5);
 
-	InitGL();
-    ReSizeGLScene(WIDTH, HEIGHT);
+	glutInit(&argc, argv);
+	glutInitDisplayMode (GLUT_SINGLE);
 
-    while (window.isOpen()) {
+	glutInitWindowSize (500, 500);
+	glutCreateWindow("Your first OpenGL Window");
 
-		handle_events(window, gravity_handler);
+	glutIdleFunc(display);
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
 
-		gravity_handler.update();
-
-		// clear the buffers
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        
-        // draw
-        DrawGLScene(gravity_handler);
-
-        // end the current frame (internally swaps the front and back buffers)
-
-		window.display();
-    }
-
-    return 0;
-}
-
-void handle_events(sf::RenderWindow& window, GravityHandler& handler){
-	sf::Event event;
-
-	// while there are pending events...
-	while (window.pollEvent(event))
-	{
-	    // check the type of the event...
-	    switch (event.type){
-	        // window closed
-	        case sf::Event::Closed:
-	            window.close();
-	            break;
-
-	        // key pressed
-	        case sf::Event::KeyPressed:
-				if (event.type == sf::Event::KeyPressed) {
-				    if (event.key.code == sf::Keyboard::Escape) {
-						window.close();
-				    }
-				    if (event.key.code == sf::Keyboard::Equal){
-						handler.set_time_multiplier(handler.get_time_multiplier() + 0.05);
-					}
-					if (event.key.code == sf::Keyboard::Dash){
-						handler.set_time_multiplier(handler.get_time_multiplier() - 0.05);
-					}
-					if (event.key.code == sf::Keyboard::R) {
-						handler.clear();
-						window.clear();
-						populate_handler(handler);
-					}
-					if (event.key.code == sf::Keyboard::C) {
-						handler.clear();
-						window.clear();
-					}	
-				}
-				break;
-			case sf::Event::MouseButtonPressed:
-				// Mouse is pressed down
-				break;
-			case sf::Event::MouseButtonReleased:
-				// Mouse has been released
-	            break;	
-	        // we don't process other types of events
-	        default:
-	            break;
-	    }
-	}
+	glutMainLoop();
 
 }
-
 
 double fRand(double fMin, double fMax) {
     double f = (double)rand() / RAND_MAX;
@@ -271,9 +57,9 @@ double fRand(double fMin, double fMax) {
 }
 
 Body random_body(){
-	Vector position = Vector(fRand(-100, 100), fRand(-100, 100), fRand(-100, -50));
+	Vector position = Vector(fRand(-20, 20), fRand(-20, 20), fRand(-40, -60));
 	//Vector velocity = Vector(fRand(0, 0.5), fRand(0, 0.5), 0);
-	double mass = pow(10, rand() % 8 + 1);
+	double mass = pow(10, 4);
 	
 	Body a(position, mass);
 	//a.set_velocity(velocity);
@@ -377,4 +163,42 @@ void make_solar_system(GravityHandler& handler){
 	Body sun(Vector(WIDTH / 2, HEIGHT / 2 , 0), sun_mass);
 	sun.set_color(255, 234, 163);
 	handler.add_body(sun);
+}
+
+void reshape(int width, int height) {
+	glViewport(0, 0, (GLsizei)width, (GLsizei)height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(60, (GLfloat)width / (GLfloat)height, 1.0, 100.0);
+	glMatrixMode(GL_MODELVIEW);
+}
+
+void display (void) {  
+
+	gravity_handler.update();
+
+	glClear (GL_COLOR_BUFFER_BIT); 
+	// Load the Identity Matrix to reset our drawing locations
+	glLoadIdentity();
+
+	// Do the drawing...
+	for (int i = 0; i < gravity_handler.get_bodies().size(); ++i){
+		draw_body(gravity_handler.get_bodies()[i]);
+	}
+
+	// Flush the OpenGL buffers to the window
+	glFlush();
+}
+
+void draw_body(Body& body){
+	glPushMatrix();
+	Vector position = body.get_position();
+	glTranslatef(position.x, position.y, position.z);
+	//glTranslatef(-5, 5, -50.0f);
+	double r = body.get_r();
+	double g = body.get_g();
+	double b = body.get_b();
+	glColor3f( r / 255.0, g / 255.0, b / 255.0);
+	glutSolidSphere(body.get_radius(), 12, 12);
+	glPopMatrix();
 }
